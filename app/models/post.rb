669f20e8f8288cf96cf03fe62a4ed520247718fc
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-25 10:59:14 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,7 +17,7 @@ class Post < ActiveRecord::Base
   end
 
   def last_updated
-  updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
-end
+    updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
+  end
 
 end
